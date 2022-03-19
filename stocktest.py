dc36059e80656f3657ff2ce1a5a6c7e5ea459ea9
CentralC <99512112+CentralC@users.noreply.github.com>
2022-03-19 13:13:54 +0100

    Update stocktest.py
    tabel formaat, oude versie staat eronder als notes

--- stocktest.py
+++ stocktest.py
@@ -1,8 +1,22 @@
 import requests
+import csv
 
+CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=64TY7285TUX7FM6P'
+
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
+
+
+#Oude Versie Onder
+        
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=64TY7285TUX7FM6P'
-r = requests.get(url)
-data = r.json()
+#url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=64TY7285TUX7FM6P'
+#r = requests.get(url)
+#data = r.json()
 
-print(data)
+#print(data)
