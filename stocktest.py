f6bc02c78b2aa7eaf6942c96b96f2b0169f7dec0
CentralC <99512112+CentralC@users.noreply.github.com>
2022-03-19 09:00:55 +0100

    Update stocktest.py

--- stocktest.py
+++ stocktest.py
@@ -1,7 +1,7 @@
 import requests
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=64TY7285TUX7FM6P'
 r = requests.get(url)
 data = r.json()
 
