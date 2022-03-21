e03ef167c6fad61975f114e87975a76290a28ca1
micmax1 <45341644+micmax1@users.noreply.github.com>
2022-03-21 23:25:38 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.pagelayout import PageLayout
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.widget import Widget
+import requests
+import csv
+
+CSV_URL = 'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=ETH&market=USD&interval=5min&apikey=64TY7285TUX7FM6P'
 
 
 class BoxLayoutApp(App):
     def build(self):
         rootbox = BoxLayout(orientation='vertical')
-        hb = BoxLayout(orientation='horizontal')
-        vb = BoxLayout(orientation='vertical')
-        scroll = ScrollView()
-        label1 = Label(text="Cuban Investor")
-        label2 = Label(text="Cuban Investor")
         button1 = Button(text="Cuban Investor")
-        button2 = Button(text="Cuban Investor")
-        vb.add_widget(label1)
-        scroll.add_widget(button1)
-        """scroll.add_widget(button2)"""
-        rootbox.add_widget(vb)
-        rootbox.add_widget(scroll)
+        rootbox.add_widget(button1)
+        button1.bind(on_press=BoxLayoutApp.API)
         return rootbox
+
+    def API(self):
+        with requests.Session() as s:
+            download = s.get(CSV_URL)
+            decoded_content = download.content.decode('utf-8')
+            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+            my_list = list(cr)
+            for row in my_list:
+                print(row)
 
 
 BoxLayoutApp().run()
