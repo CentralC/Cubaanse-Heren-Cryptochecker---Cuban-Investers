d2f877636fcf687df265ac8f8a88ebaa310d4966
micmax1 <45341644+micmax1@users.noreply.github.com>
2022-03-22 14:07:45 +0100

    Update Kivy testapp.py

--- code/Kivy testapp.py
+++ code/Kivy testapp.py
@@ -1,1 +1,28 @@
-pass
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.widget import Widget
+
+
+class BoxLayoutApp(App):
+    def build(self):
+        rootbox = BoxLayout(orientation='vertical')
+        hb = BoxLayout(orientation='horizontal')
+        vb = BoxLayout(orientation='vertical')
+        scroll = ScrollView()
+        label1 = Label(text="Cuban Investor")
+        label2 = Label(text="Cuban Investor")
+        button1 = Button(text="Cuban Investor")
+        button2 = Button(text="Cuban Investor")
+        vb.add_widget(label1)
+        scroll.add_widget(button1)
+        """scroll.add_widget(button2)"""
+        rootbox.add_widget(vb)
+        rootbox.add_widget(scroll)
+        return rootbox
+
+
+BoxLayoutApp().run()
